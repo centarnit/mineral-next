8b5feb23232e4fe6e6ecb4657f1ae17cdf102e46
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-13 18:51:05 +0200

    material pages

--- pages/materials/[variation]/[item]/index.js
+++ pages/materials/[variation]/[item]/index.js
@@ -1,49 +1,53 @@
-import { MaterialItemFull } from "@components/Materials/MaterialsUtils/MaterialsItemFull";
+// import { MaterialItemFull } from "@components/Materials/MaterialsUtils/MaterialsItemFull";
 
-export default function Materials({ data }) {
-    if (!data) {
-        return <div>Loading...</div>;
-    }
+// export default function Materials({ data }) {
+//     if (!data) {
+//         return <div>Loading...</div>;
+//     }
 
-    return <MaterialItemFull data={data} />;
-}
+//     return <MaterialItemFull data={data} />;
+// }
 
-export async function getStaticPaths() {
-    const res = await fetch(
-        "https://mineral-backend.centarnit.live/material_group"
-    );
-    const data = await res.json();
+// export async function getStaticPaths() {
+//     const res = await fetch(
+//         "https://mineral-backend.centarnit.live/material_group"
+//     );
+//     const data = await res.json();
 
-    const paths = data.reduce((acc, material) => {
-        const variations = material.items.map((item) => ({
-            params: {
-                variation: material.name,
-                item: item.name,
-            },
-        }));
-        return [...acc, ...variations];
-    }, []);
+//     const paths = data.reduce((acc, material) => {
+//         const variations = material.items.map((item) => ({
+//             params: {
+//                 variation: material.name,
+//                 item: item.name,
+//             },
+//         }));
+//         return [...acc, ...variations];
+//     }, []);
 
-    return { paths, fallback: false };
-}
+//     return { paths, fallback: false };
+// }
 
-export async function getStaticProps({ params }) {
-    const { variation, item } = params;
+// export async function getStaticProps({ params }) {
+//     const { variation, item } = params;
 
-    const res = await fetch(
-        `https://mineral-backend.centarnit.live/material_group/`
-    );
-    const data = await res.json();
+//     const res = await fetch(
+//         `https://mineral-backend.centarnit.live/material_group/`
+//     );
+//     const data = await res.json();
 
-    // find the material group with the same name as the variation param
-    const materialGroup = data.find((group) => group.name === variation);
+//     // find the material group with the same name as the variation param
+//     const materialGroup = data.find((group) => group.name === variation);
 
-    // find the material item with the same name as the item param
-    const materialItem = materialGroup.items.find((i) => i.name === item);
+//     // find the material item with the same name as the item param
+//     const materialItem = materialGroup.items.find((i) => i.name === item);
 
-    return {
-        props: {
-            data: materialItem,
-        },
-    };
+//     return {
+//         props: {
+//             data: materialItem,
+//         },
+//     };
+// }
+
+export default function MaterialsItem() {
+    return <div>Materials</div>;
 }
--- pages/materials/[variation]/index.js
+++ pages/materials/[variation]/index.js
@@ -1,40 +1,44 @@
-import { MaterialsGroup } from "@components/Materials/MaterialsUtils/MaterialsGroup";
+// import { MaterialsGroup } from "@components/Materials/MaterialsUtils/MaterialsGroup";
 
-export default function Materials({ data }) {
-    if (!data) {
-        return <div>Loading...</div>;
-    }
+// export default function Materials({ data }) {
+//     if (!data) {
+//         return <div>Loading...</div>;
+//     }
 
-    return <MaterialsGroup data={data} />;
-}
+//     return <MaterialsGroup data={data} />;
+// }
 
-export async function getStaticPaths() {
-    const res = await fetch(
-        "https://mineral-backend.centarnit.live/material_group/"
-    );
-    const data = await res.json();
-    if (!Array.isArray(data)) {
-        return { paths: [], fallback: false };
-    }
-    const paths = data.map((material) => ({
-        params: { variation: material.name },
-    }));
+// export async function getStaticPaths() {
+//     const res = await fetch(
+//         "https://mineral-backend.centarnit.live/material_group/"
+//     );
+//     const data = await res.json();
+//     if (!Array.isArray(data)) {
+//         return { paths: [], fallback: false };
+//     }
+//     const paths = data.map((material) => ({
+//         params: { variation: material.name },
+//     }));
 
-    return { paths, fallback: false };
-}
+//     return { paths, fallback: false };
+// }
 
-export async function getStaticProps({ params }) {
-    const res = await fetch(
-        `https://mineral-backend.centarnit.live/material_group/`
-    );
-    const data = (await res.json()).filter(
-        (item) => item.name === params?.variation // use optional chaining to avoid errors
-    )[0];
+// export async function getStaticProps({ params }) {
+//     const res = await fetch(
+//         `https://mineral-backend.centarnit.live/material_group/`
+//     );
+//     const data = (await res.json()).filter(
+//         (item) => item.name === params?.variation // use optional chaining to avoid errors
+//     )[0];
 
-    return {
-        props: {
-            data: data,
-            params,
-        },
-    };
+//     return {
+//         props: {
+//             data: data,
+//             params,
+//         },
+//     };
+// }
+
+export default function MaterialsGroup() {
+    return <div>Materials</div>;
 }
