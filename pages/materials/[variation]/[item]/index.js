29fa6da53d42a0ba151beb5fb58d8b9be6808b6b
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-13 18:42:28 +0200

    material pages

--- pages/materials/[variation]/[item]/index.js
+++ pages/materials/[variation]/[item]/index.js
@@ -43,7 +43,7 @@ export async function getStaticProps({ params }) {
 
     return {
         props: {
-            data: materialItem || null,
+            data: materialItem,
         },
     };
 }
