5fe828b9af440d11760c1cc68e4e3a54bf8959dc
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-18 12:39:37 +0200

    image loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const withImgix = require("next-imgix");
+
+module.exports = withImgix({
     reactStrictMode: true,
     runtime: "edge",
     images: {
+        loader: "imgix",
+        path: "https://your-imgix-domain.imgix.net/",
         domains: ["mineral-backend.centarnit.live"],
     },
-};
+});
