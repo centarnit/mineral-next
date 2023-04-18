f0e885a71e905b9e8e550896ee3e794fbbb31152
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-18 13:08:41 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 module.exports = {
     reactStrictMode: true,
-    runtime: "edge",
     images: {
         domains: ["mineral-backend.centarnit.live"],
-        loader: "default",
         unoptimized: true,
     },
 };
