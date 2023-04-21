bdf7f1f722316982a51b8d9f04625c8818a49510
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-21 07:41:18 +0200

    removed loader

--- components/Materials/MaterialsUtils/MaterialsGroup.jsx
+++ components/Materials/MaterialsUtils/MaterialsGroup.jsx
@@ -1,22 +1,13 @@
 import Link from "next/link";
 import NextImage from "next/image";
 
-const sanityIoImageLoader = ({ src, width, quality }) => {
-    return `https://cdn.sanity.io/${src}?w=${width}&q=${quality || 75}`;
-};
-
 const Item = (props) => {
     return (
         <Link
             href={`/materials/${props.group}/${props.data.name}`}
             className="materials-group-item"
         >
-            <NextImage
-                src={props.data.front_image}
-                width={100}
-                height={100}
-                loader={sanityIoImageLoader}
-            />
+            <NextImage src={props.data.front_image} width={100} height={100} />
             <h1>{props.data.name}</h1>
         </Link>
     );
